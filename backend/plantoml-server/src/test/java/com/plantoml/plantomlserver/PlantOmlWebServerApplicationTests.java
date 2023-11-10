cf6301c4d9b5434c33dc605e528202ca163091de
Emmett Cocke <emmettlsc@gmail.com>
2023-11-09 16:12:29 -0800

    #9 implemented simple backend accepting requests of OML encoded in hex, plantUML style, and deflate + base64

---
+++ backend/plantoml-server/src/test/java/com/plantoml/plantomlserver/PlantOmlWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantoml.plantomlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantOmlWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
