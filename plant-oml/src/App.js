c4bcb46e14b2e6828784e52f8779256f9c42509d
Michael R <mr2004@g.ucla.edu>
2023-11-28 17:13:57 -0800

    Add image display

--- plant-oml/src/App.js
+++ plant-oml/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import { Buffer } from 'buffer';
 import React  from 'react';
+import logo from './logo.svg'
+import imgresult from './img.jpeg'
+
+const imageRef = React.createRef();
 
 function App() {
   console.log("start");
@@ -28,6 +32,7 @@ function App() {
           </label>
           <input type="submit" value="Submit" />
         </form>
+        <img src={logo} alt="logo" ref={imageRef} name="imgdisplay" />
       </header>
     </div>
   );
@@ -38,23 +43,30 @@ const printOMLNS = (f) =>{
   var omlcode = f.target.omlcode.value;
   f.target.omlcode.value = "";
   const hex = Buffer.from(omlcode, 'utf8').toString('hex');
-  console.log(hex)
+  renderOMLDiagram(hex);
 }
 
 const printOMLHEX = (f) =>{
   f.preventDefault();
   var omlcode = f.target.omlcode.value;
   f.target.omlcode.value = "";
-  console.log(omlcode)
+  renderOMLDiagram(omlcode);
 }
 
 const printOMLDS = (f) =>{
   f.preventDefault();
+  var deflate = require('deflate-js')
   var omlcode = f.target.omlcode.value;
   f.target.omlcode.value = "";
-  const hex = Buffer.from(omlcode, 'utf8').toString('hex');
-  console.log(hex)
+  var decompressed = deflate.inflate(omlcode);
+  const hex = Buffer.from(decompressed, 'utf8').toString('hex');
+  renderOMLDiagram(hex);
 }
 
+
+const renderOMLDiagram = (hex) => {
+  console.log(hex);
+  imageRef.current.src = imgresult
+}
 
 export default App;
